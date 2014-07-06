9a48bb1c576d652103e73354b5ed6cb084596878
ueki.popngfdmddr@gmail.com <Ueki@Ueki-PC.(none)>
2014-07-06 17:57:56 +0900

    refactor testcase reading module

--- CF_tester/test.rb
+++ CF_tester/test.rb
@@ -12,53 +12,50 @@ def test(file, input, output)
   end
 end
 
+def read_testcase(filename)
+  input = ""
+  output = ""
+  inputs = []
+  outputs = []
+  f_input = false
+  f_output = false
+  IO.foreach(filename) do |line|
+    if(line == "input\n")
+      f_input = true
+      f_output = false
+      if input != ""
+        inputs.push input
+        outputs.push output
+      end
+      input = ""
+      output = ""
+    elsif(line == "output\n")
+      f_input = false
+      f_output = true
+    elsif f_input
+      input += line
+    elsif f_output
+      output += line
+    end
+  end
+  inputs.push input
+  outputs.push output
+  return [inputs, outputs]
+end
+
 if ARGV.length < 1 && ARGV.length > 2
   puts "usage: test.rb {problem name} ({case #})"
   exit
 end
 
-if ARGV.length < 2
-  try = -1
-else
-  try = ARGV[1].to_i
-end
+try = (ARGV.length<2 ? -1 : ARGV[1].to_i)
 
-# initialize
-input = ""
-output = ""
-inputs = []
-outputs = []
-f_input = false
-f_output = false
+testcase = read_testcase("#{ARGV[0]}.txt")
 
-#testcase read loop
-IO.foreach("#{ARGV[0]}.txt") do |line|
-  if(line == "input\n")
-    f_input = true
-    f_output = false
-    if input != ""
-      inputs.push input
-      outputs.push output
-    end
-    input = ""
-    output = ""
-  elsif(line == "output\n")
-    f_input = false
-    f_output = true
-  elsif f_input
-    input += line
-  elsif f_output
-    output += line
-  end
-end
-
-inputs.push input
-outputs.push output
-
-for i in 0..(inputs.length-1) do
+for i in 0..(testcase.length-1) do
   if (try < 0 || try == i+1)
     print "case #" + (i+1).to_s + " : "
-    test(ARGV[0], inputs[i], outputs[i])
+    test(ARGV[0], testcase[0][i], testcase[1][i])
   end
 end
 
